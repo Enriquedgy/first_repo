aad12e6f85f78ca3742214100d3c380319b19a3a
Enrique de Galdo Yunta <enrique.s234489@cesurformacion.com>
2021-12-17 19:01:47 +0100

    He actualizado las líneas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
-    print('Mi primera línea de código')
-    print('Mi segunda línea de código')
-    print('Mi tercera línea de código')
+    print('Mi número 1 línea de código')
+
+    print('Mi cuarta línea de código')
